36def9c7563f253eb816db51f3fa3a8cc58cc138
trinath.swain <trinath.967.swain@gmail.com>
2023-10-15 13:29:59 +0530

    WRKSHOP-101: Aadded directivePOC

---
+++ projects/amazon/src/app/directivespoc/directivespoc.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-directivespoc',
+  templateUrl: './directivespoc.component.html',
+  styleUrls: ['./directivespoc.component.css']
+})
+export class DirectivespocComponent {
+
+}
